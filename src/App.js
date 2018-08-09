bfff3e57c366560f92be6b05ae7d6e664242bc79
Norman <revel@normans-mbp.lan>
2018-08-09 00:31:40 -0600

    fixed background and the positions of other elements

--- src/App.js
+++ src/App.js
@@ -15,18 +15,37 @@ class App extends Component {
 
       <div className="App-background">
 
-        <header className="App-header">
-        {/*  <img src={logo} className="App-logo" alt="logo" /> */}
-        </header>
+
         <div className="App-bar-div">
           <img src={navBar} className="App-bar" alt="logo" />
           <IconCircle />
-          <IconCode style={{top: '105px'}}/>
-          <IconAbout style={{top: '170px'}}/>
-          <IconMusic style={{top: '235px'}}/>
-          <IconInfo style={{top: '300px'}}/>
+          <IconCode style={{top: '-90px'}}/>
+          <IconAbout style={{top: '-25px'}}/>
+          <IconMusic style={{top: '40px'}}/>
+          <IconInfo style={{top: '105px'}}/>
         </div>
+
+        {/*<h1 className="App-title">Wellocme to my portfolio</h1>*/}
         <h1 className="App-title">ERIC NORMAN</h1>
+        <h1 className="App-title2" style={{float:'left',top:'130px',left:'50%'}}>Software Engineer</h1>
+        <h1 className="App-title2" >Portfolio</h1>
+        <h1 className="App-title" style={{top:'1000px'}}>Next page</h1>
+{/*
+        Find out more about me at my website:
+        http://www.adamhartwig.co.uk
+
+        I'm an award winning digital designer and developer specialising in rich interactive experiences for web, tablet and mobile. I'm passionate about interactive media, and in an age of digital, I believe being able to bring design to life with code is essential.
+
+        Since graduating with a 1st Class BA Honours degree in Design: Multimedia & Graphics in 2008, I have been working with global brands, and I am internationally recognised in the creative industry for my talent and ability to intertwine design and development philosophies.
+
+        Specialities:
+        Concept to creation
+        Web, mobile and tablet design and development
+        Rich interactive applications
+        Video production
+        User experience
+        Innovation
+*/}
       </div>
     );
   }
